4f6b4e128f3607b220de8d4341d83dbc0729f16b
pkarasev@eos-orthopaedics.com <pkarasev@eos-orthopaedics.com>
2016-03-15 18:12:18 -0400

    make the spawner more aggressive

--- tools/run-pk-btgatt-hack.sh
+++ tools/run-pk-btgatt-hack.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
+#export PATH=`pwd`:$PATH
+exe_name=pk-btgatt-hack
 
-while pk-btgatt-hack $* ; do
-   echo "Relaunching..."
+if [ -f $exe_name ]
+then
+  exe_name=./$exe_name
+  echo "setting exe name: $exe_name"
+  sleep 0.5
+else
+  echo "ok, using $exe_name from the path..."
+fi
+
+echo $exe_name $*
+
+while $exe_name  $* ; do
+   echo "***!!! Relaunching..."
 done
-	
+
+SpawnStopToken="spawn-token.tmp"
+echo "executable finished; Respawning unless file:"
+echo "                 ***** $SpawnStopToken ***** "
+echo "                                exists in `pwd` ."	
+sleep 1
+
+if [ ! -f $SpawnStopToken ]
+then
+  . run-pk-btgatt-hack.sh $* & 
+fi
+
+echo "done with runner instance"
+
